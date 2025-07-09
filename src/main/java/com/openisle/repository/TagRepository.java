d337eca9ab53b1aca127d85ab17f15686f169b71
tim <nagisa12164@163.com>
2025-07-09 16:11:04 +0800

    gh page build 0.1

--- src/main/java/com/openisle/repository/TagRepository.java
+++
@@ -1,7 +0,0 @@
-package com.openisle.repository;
-
-import com.openisle.model.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-}
